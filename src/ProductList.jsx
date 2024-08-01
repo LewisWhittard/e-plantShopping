bcce6f3b91166c3d5b2ba7728c65130f73bcea68
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2024-08-01 23:22:30 +0100

    EPS - Add to cart management

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -304,7 +304,12 @@ const handlePlantsClick = (e) => {
                                         <img className="product-image" src={plant.image} alt={plant.name} />
                                         <div className="product-title">{plant.name}</div>
                                         {/*Similarly like the above plant.name show other details like description and cost*/}
-                                        <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                        // if the product is already added to cart, show "Added to Cart" button, else show "Add to Cart" button
+                                        {addedToCart[plant.name] ? (
+                                            <button disabled={true} className="product-button" onClick={() => handleAddToCart(plant)}>Added to Cart</button>
+                                        ) : (
+                                            <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                        )}
                                     </div>
                                 ))}
                             </div>
